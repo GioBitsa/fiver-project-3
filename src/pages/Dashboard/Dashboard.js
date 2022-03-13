d34aa5593bd4e20d41a9d7d2e0dcd0c37fcd4fe5
Giorgi <giorgi.bitsadze01@gmail.com>
2022-03-14 02:59:22 +0400

    added some styles

---
+++ src/pages/Dashboard/Dashboard.js
@@ -0,0 +1,252 @@
+import * as React from 'react';
+import { styled, useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import SearchIcon from '@mui/icons-material/Search';
+import PeopleIcon from '@mui/icons-material/People';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import { Avatar, Badge, Tab, Tabs } from '@mui/material';
+import { PhonelinkLockOutlined } from '@mui/icons-material';
+
+const drawerWidth = 240;
+const smallDrawerWidth = 90;
+
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
+    ({ theme, open }) => ({
+        flexGrow: 1,
+        padding: theme.spacing(3),
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        marginLeft: `-${drawerWidth}px`,
+        ...(open && {
+            transition: theme.transitions.create('margin', {
+                easing: theme.transitions.easing.easeOut,
+                duration: theme.transitions.duration.enteringScreen,
+            }),
+            marginLeft: 0,
+        }),
+    }),
+);
+
+const AppBar = styled(MuiAppBar, {
+    shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+    paddingLeft: `${smallDrawerWidth}px`,
+    transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    ...(open && {
+        width: `calc(100% - ${smallDrawerWidth}px - ${drawerWidth}px)`,
+        marginLeft: `${drawerWidth}px`,
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    }),
+}));
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'center',
+}));
+
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`vertical-tabpanel-${index}`}
+            aria-labelledby={`vertical-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{ p: 3 }}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+
+function a11yProps(index) {
+    return {
+        id: `vertical-tab-${index}`,
+        'aria-controls': `vertical-tabpanel-${index}`,
+    };
+}
+
+const Dashboard = () => {
+    const theme = useTheme();
+    const [open, setOpen] = React.useState(true);
+    const [value, setValue] = React.useState(0);
+
+    const handleDrawerOpen = () => {
+        setOpen(!open);
+    };
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    return (
+        <Box sx={{ display: 'flex' }}>
+            <CssBaseline />
+            <AppBar position="fixed" open={open}>
+                <Toolbar>
+                    <Typography variant="h6" sx={{ flexGrow: 1 }} noWrap component="div">
+                        Persistent drawer
+                    </Typography>
+                    <IconButton aria-label="search" color="inherit">
+                        <SearchIcon />
+                    </IconButton>
+                    <IconButton aria-label="group" color="inherit">
+                        <PeopleIcon />
+                    </IconButton>
+                    <IconButton
+                        color="inherit"
+                    >
+                        <Badge badgeContent={17} color="error">
+                            <NotificationsIcon />
+                        </Badge>
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                sx={{
+                    width: smallDrawerWidth,
+                    flexShrink: 0,
+                    '& .MuiDrawer-paper': {
+                        width: smallDrawerWidth,
+                        boxSizing: 'border-box',
+                        justifyContent: 'space-between',
+                    },
+                    zIndex: 9999,
+                    height: '100vh',
+                }}
+                variant="persistent"
+                anchor="left"
+                open={true}
+            >
+                <DrawerHeader>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={handleDrawerOpen}
+                        edge="start"
+                        sx={{ ml: 'auto', mr: 'auto' }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                </DrawerHeader>
+                <Tabs
+                    orientation="vertical"
+                    variant="scrollable"
+                    value={value}
+                    onChange={handleChange}
+                    aria-label="Vertical tabs example"
+                    sx={{ borderRight: 0, borderColor: 'divider' }}
+                >
+                    <Tab icon={<PhonelinkLockOutlined />} aria-label="phone" {...a11yProps(0)} />
+                    <Tab icon={<PhonelinkLockOutlined />} aria-label="phone" {...a11yProps(1)} />
+                    <Tab icon={<PhonelinkLockOutlined />} aria-label="phone" {...a11yProps(2)} />
+                </Tabs>
+                <DrawerHeader>
+                    <Avatar>H</Avatar>
+                </DrawerHeader>
+            </Drawer>
+            <Drawer
+                sx={{
+                    width: drawerWidth,
+                    flexShrink: 0,
+                    '& .MuiDrawer-paper': {
+                        width: drawerWidth,
+                        boxSizing: 'border-box',
+                        marginLeft: `${smallDrawerWidth}px`,
+                    },
+                }}
+                variant="persistent"
+                anchor="left"
+                open={open}
+            >
+                <DrawerHeader>
+                    <Typography>Portfolio</Typography>
+                </DrawerHeader>
+                <Divider />
+                <TabPanel value={value} index={0}>
+                    <List>
+                        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                            <ListItem button key={text}>
+                                <ListItemIcon>
+                                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                                </ListItemIcon>
+                                <ListItemText primary={text} />
+                            </ListItem>
+                        ))}
+                    </List>
+                </TabPanel>
+                <TabPanel value={value} index={1}>
+                    Item Two
+                </TabPanel>
+                <TabPanel value={value} index={2}>
+                    Item Three
+                </TabPanel>
+            </Drawer>
+            <Main open={open}>
+                <DrawerHeader />
+                <Typography paragraph>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
+                    enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
+                    imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
+                    Convallis convallis tellus id interdum velit laoreet id donec ultrices.
+                    Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+                    adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
+                    nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
+                    leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
+                    feugiat vivamus at augue. At augue eget arcu dictum varius duis at
+                    consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
+                    sapien faucibus et molestie ac.
+                </Typography>
+                <Typography paragraph>
+                    Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
+                    eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
+                    neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
+                    tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
+                    sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
+                    tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
+                    gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
+                    et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
+                    tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
+                    eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
+                    posuere sollicitudin aliquam ultrices sagittis orci a.
+                </Typography>
+            </Main>
+        </Box>
+    );
+}
+
+export default Dashboard;
